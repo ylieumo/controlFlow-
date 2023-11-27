6d3f65222cd4558b4dce48f3a0a23d95e487f422
ylieumo <yoldjampou@gmail.com>
2023-11-27 18:45:35 -0500

    my control flow

---
+++ index1.js
@@ -0,0 +1,44 @@
+const PI = 3.1415;
+const area = PI * radius * radius; 
+
+
+// Function to calculate the area of a circle
+function calculateArea(radius) {
+   PI * radius * radius;
+}
+
+// Function to predict plant growth after a specific number of weeks
+function predictPlantGrowth(initialPlants, weeks) {
+    initialPlants * 2^weeks;
+}
+
+// control flow to make decisions on whether the plants should be:
+
+    if (maxCapacity > 80) {
+        return "Prune";
+    } else if ( maxCapacity >= 50 &&  maxCapacity <= 80) {
+        return "Monitor";
+    } else {
+        return "Plant";
+    }
+
+
+
+// Initial conditions
+const radius = 5;
+const maxCapacity = calculateArea(radius) / 0.8;
+const initialPlants = 20;
+
+// Results for 1, 2, and 3 weeks of growth
+for (let weeks = 1; weeks <= 3; weeks++) {
+    const currentPlants = predictPlantGrowth(initialPlants, weeks);{
+        initialPlants * 2^weeks;
+    }
+
+
+    //  amount of additional space that would be required
+    function calculateAdditionalSpace(initialPlants, weeks, currentRadius) {
+        const finalPlants = predictPlantGrowth(initialPlants, weeks);
+        const finalMaxCapacity = calculateArea(currentRadius) / 0.8;
+    }
+    
